75ddf6752e2e79c7de2928ad2e1e53d0a4520d69
Wei Feng <weicongf73@gmail.com>
2020-06-11 20:49:49 -0700

    new file:   Aura Components Basic/Expenses/ExpensesController.apxc 	modified:   Aura Components Basic/Expenses/expenses.cmp 	modified:   Aura Components Basic/Expenses/expensesController.js 	new file:   Traihead/Materials/Aura Components Basics/Expenses/ExpensesController.apxc 	modified:   Traihead/Materials/Aura Components Basics/Expenses/expenses.cmp 	modified:   Traihead/Materials/Aura Components Basics/Expenses/expensesController.js

--- Traihead/Materials/Aura Components Basics/Expenses/expensesController.js
+++ Traihead/Materials/Aura Components Basics/Expenses/expensesController.js
@@ -1,4 +1,22 @@
 ({
+    // Load expenses from Salesforce
+    doInit: function(component, event, helper) {
+        // Create the action
+        var action = component.get("c.getExpenses");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.expenses", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+    },
+    
     clickCreate: function(component, event, helper) {
         var validExpense = component.find('expenseform').reduce(function (validSoFar, inputCmp) {
             // Displays error messages for invalid fields
